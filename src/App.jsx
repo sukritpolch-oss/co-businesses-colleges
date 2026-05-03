30288e064305bd1b99fdfce7e625dd5e18b8339b
Sukritpol-Chotautthapol <sukritpol.ch@gmail.com>
2026-05-04 01:07:41 +0700

    Fix white screen3

--- src/App.jsx
+++ src/App.jsx
@@ -1249,7 +1249,7 @@ const App = () => {
     { id: 'reports', baseLabel: 'รายงาน', i: FileText },
     { id: 'evaluation', baseLabel: 'แบบประเมิน', i: ClipboardCheck },
     { id: 'share', baseLabel: 'แชร์แผนฝึก', i: UploadCloud },
-    { id: 'cloud', baseLabel: 'คลังส่วนกลาง', i: Cloud }, // ให้ครูฝึกเห็นด้วยตามที่ร้องขอ
+    { id: 'cloud', baseLabel: 'คลังงาน', i: Cloud }, // ให้ครูฝึกเห็นด้วยตามที่ร้องขอ
     { id: 'feedback', baseLabel: 'ประเมินระบบ', i: Star },
     ...(isDeveloper ? [{ id: 'admin', baseLabel: 'จัดการผู้ใช้', i: Users }] : [])
   ].map((item, index) => ({
@@ -1263,8 +1263,8 @@ const App = () => {
   if (!isAuthenticated) {
     return (
       <div className="min-h-screen bg-slate-100 flex flex-col items-center justify-center font-serif p-4 relative overflow-hidden">
-        <div className="absolute top-[-10%] left-[-10%] w-96 h-96 bg-indigo-400 rounded-full mix-blend-multiply filter blur-3xl opacity-20"></div>
-        <div className="absolute bottom-[-10%] right-[-10%] w-96 h-96 bg-blue-400 rounded-full mix-blend-multiply filter blur-3xl opacity-20"></div>
+        <div className="absolute top-[-10%] left-[-10%] w-96 h-96 bg-indigo-300 rounded-full mix-blend-multiply filter blur-3xl opacity-20"></div>
+        <div className="absolute bottom-[-10%] right-[-10%] w-96 h-96 bg-blue-300 rounded-full mix-blend-multiply filter blur-3xl opacity-20"></div>
 
         <div className="w-full max-w-md bg-white p-8 md:p-10 rounded-3xl border border-slate-200 shadow-2xl animate-in zoom-in-95 duration-500 z-10 relative">
           <div className="flex justify-center mb-6">
